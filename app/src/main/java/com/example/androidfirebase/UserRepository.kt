2db27c11d69a2f019147362c6623e5f70cbb60fe
Space-Belt <dngurwn@gmail.com>
2025-04-24 18:55:41 +0900

    feat: 레포지토리, 회원관련뷰모델 추가, auth결과 클래스 추가, MainActivity에 navigationGraph 추가

---
+++ app/src/main/java/com/example/androidfirebase/UserRepository.kt
@@ -0,0 +1,42 @@
+package com.example.androidfirebase
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+class UserRepository(private val auth: FirebaseAuth,
+                     private val firestore: FirebaseFirestore
+) {
+    private suspend fun saveUserToFirestore(user: User) {
+        firestore.collection("users").document(user.email).set(user).await()
+    }
+
+    suspend fun signUp(email: String, password: String, firstName: String, lastName: String): Result<Boolean> =
+        try {
+            auth.createUserWithEmailAndPassword(email, password).await()
+            //add user to firestore
+            val user = User(firstName, lastName, email)
+            saveUserToFirestore(user)
+            Result.Success(true)
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+
+    suspend fun login(email: String, password: String): Result<Boolean> =
+        try {
+            auth.signInWithEmailAndPassword(email, password).await()
+            Result.Success(true)
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
+}
+
+object Injection {
+    private val instance: FirebaseFirestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
+
+    fun instance(): FirebaseFirestore {
+        return instance
+    }
+}

565f966c81a210de025cbaec5f2ee15761d07770
Space-Belt <dngurwn@gmail.com>
2025-04-26 17:56:00 +0900

    feat: 시간관련 util 파일 생성, 추가된파일들

---
+++ app/src/main/java/com/example/androidfirebase/utils/DateTimeUtils.kt
@@ -0,0 +1,39 @@
+package com.example.androidfirebase.utils
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun formatTimestamp(timestamp: Long): String {
+    val messageDateTime =
+        LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault())
+    val now = LocalDateTime.now()
+
+    return when {
+        isSameDay(messageDateTime, now) -> "오늘 ${formatTime(messageDateTime)}"
+        isSameDay(messageDateTime.plusDays(1), now) -> "어제 ${formatTime(messageDateTime)}"
+        else -> formatDate(messageDateTime)
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun isSameDay(dateTime1: LocalDateTime, dateTime2: LocalDateTime): Boolean {
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    return dateTime1.format(formatter) == dateTime2.format(formatter)
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun formatTime(dateTime: LocalDateTime): String {
+    val formatter = DateTimeFormatter.ofPattern("a hh:mm")
+    return formatter.format(dateTime)
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun formatDate(dateTime: LocalDateTime): String {
+    val formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일")
+    return formatter.format(dateTime)
+}

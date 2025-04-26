9edb770cae5dae310aa1ef7af22c7190fabce4c5
Space-Belt <dngurwn@gmail.com>
2025-04-26 17:53:28 +0900

    feat: UID 추가로 유저 인식못하는문제 해결

--- app/src/main/java/com/example/androidfirebase/UserRepository.kt
+++ app/src/main/java/com/example/androidfirebase/UserRepository.kt
@@ -1,5 +1,8 @@
 package com.example.androidfirebase
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
@@ -7,18 +10,50 @@ import kotlinx.coroutines.tasks.await
 class UserRepository(private val auth: FirebaseAuth,
                      private val firestore: FirebaseFirestore
 ) {
+    private val _messages = MutableLiveData<List<Message>>()
+    val messages: LiveData<List<Message>> get() = _messages
+
+    private val _roomId = MutableLiveData<String>()
+    private val _currentUser = MutableLiveData<User>()
+    val currentUser: LiveData<User> get() = _currentUser
+
+    suspend fun getCurrentUser(): Result<User> = try {
+        val firebaseUser = auth.currentUser ?: throw IllegalStateException("Not logged in")
+
+        val uid = firebaseUser.uid
+        val document = firestore.collection("users")
+            .document(firebaseUser.uid) // ✅ email 대신 uid 사용
+            .get()
+            .await()
+
+        if (!document.exists()) {
+            throw IllegalStateException("Document not found for UID: $uid")
+        }
+
+        val user = document.toObject(User::class.java)
+            ?: throw IllegalStateException("User data not found")
+
+        _currentUser.postValue(user) // LiveData 업데이트
+        Result.Success(user)
+    } catch (e: Exception) {
+        Result.Error(e)
+    }
+
     private suspend fun saveUserToFirestore(user: User) {
-        firestore.collection("users").document(user.email).set(user).await()
+        firestore.collection("users").document(user.uid).set(user).await()
     }
 
     suspend fun signUp(email: String, password: String, firstName: String, lastName: String): Result<Boolean> =
         try {
-            auth.createUserWithEmailAndPassword(email, password).await()
+//            auth.createUserWithEmailAndPassword(email, password).await()
+            val authResult = auth.createUserWithEmailAndPassword(email, password).await()
             //add user to firestore
-            val user = User(firstName, lastName, email)
+            val user = User(firstName, lastName, email,
+                uid = authResult.user?.uid ?: throw IllegalStateException("UID is null"))
             saveUserToFirestore(user)
             Result.Success(true)
         } catch (e: Exception) {
+            Log.e("유저레포지토리!!!!", "getCurrentUser failed: ${e.stackTraceToString()}")
             Result.Error(e)
         }
 

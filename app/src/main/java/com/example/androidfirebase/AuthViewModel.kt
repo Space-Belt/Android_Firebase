dbd687497798195f8198c99b3b5e698877d1afbe
Space-Belt <dngurwn@gmail.com>
2025-05-22 16:27:51 +0900

    feat: AuthViewModel signUp, login 예외처리 추가

--- app/src/main/java/com/example/androidfirebase/AuthViewModel.kt
+++ app/src/main/java/com/example/androidfirebase/AuthViewModel.kt
@@ -13,19 +13,34 @@ class AuthViewModel : ViewModel() {
         FirebaseAuth.getInstance(),
         Injection.instance()
     )
+    private val _errorMessage = MutableLiveData<String?>()
+    val errorMessage: LiveData<String?> get() = _errorMessage
 
     private val _authResult = MutableLiveData<Result<Boolean>>()
     val authResult: LiveData<Result<Boolean>> get() = _authResult
 
     fun signUp(email: String, password: String, firstName: String, lastName: String) {
         viewModelScope.launch {
-            _authResult.value = userRepository.signUp(email, password, firstName, lastName)
+//            _authResult.value = userRepository.signUp(email, password, firstName, lastName)
+            val result = userRepository.signUp(email, password, firstName, lastName)
+            _authResult.value = result
+            if (result is Result.Error) {
+                _errorMessage.value = result.exception.message
+            } else {
+                _errorMessage.value = null
+            }
         }
     }
 
     fun login(email: String, password: String) {
         viewModelScope.launch {
-            _authResult.value = userRepository.login(email, password)
+            val result = userRepository.login(email, password)
+            _authResult.value = result
+            if (result is Result.Error) {
+                _errorMessage.value = result.exception.message
+            } else {
+                _errorMessage.value = null
+            }
             Log.d("에러에러에러", "${_authResult.value}")
         }
     }

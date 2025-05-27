3f9cb4625a5d087833e4763db67ecfb0fe405471
Space-Belt <dngurwn@gmail.com>
2025-05-27 21:33:51 +0900

    refactoring: Navigation의 ChatRoomListScreen에 로그아웃 Unit 함수 전달

--- app/src/main/java/com/example/androidfirebase/MainActivity.kt
+++ app/src/main/java/com/example/androidfirebase/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.androidfirebase
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -17,6 +19,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.androidfirebase.ui.theme.AndroidFirebaseTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,7 +30,11 @@ class MainActivity : ComponentActivity() {
             val context = LocalContext.current
             AndroidFirebaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    NavigationGraph(navController = navController, authViewModel = authViewModel, roomViewModel = roomViewModel)
+                    NavigationGraph(
+                        navController = navController,
+                        authViewModel = authViewModel,
+                        roomViewModel = roomViewModel
+                    )
                 }
             }
         }
